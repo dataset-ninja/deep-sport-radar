758481e443bebf902c7c1a74d4d3dc394428ab54
peixed351 <peixed351@gmail.com>
2024-02-27 13:55:55 +0300

    add settings

--- src/convert.py
+++ src/convert.py
@@ -1,12 +1,22 @@
-import supervisely as sly
+import glob
 import os
-from dataset_tools.convert import unpack_if_archive
-import src.settings as s
-from urllib.parse import unquote, urlparse
-from supervisely.io.fs import get_file_name, get_file_size
 import shutil
+from urllib.parse import unquote, urlparse
 
+import numpy as np
+import supervisely as sly
+from cv2 import connectedComponents
+from dataset_tools.convert import unpack_if_archive
+from supervisely.io.fs import (
+    file_exists,
+    get_file_ext,
+    get_file_name,
+    get_file_name_with_ext,
+)
 from tqdm import tqdm
+
+import src.settings as s
+
 
 def download_dataset(teamfiles_dir: str) -> str:
     """Use it for large datasets to convert them on the instance"""
@@ -29,7 +39,7 @@ def download_dataset(teamfiles_dir: str) -> str:
             total=fsize,
             unit="B",
             unit_scale=True,
-        ) as pbar:        
+        ) as pbar:
             api.file.download(team_id, teamfiles_path, local_path, progress_cb=pbar)
         dataset_path = unpack_if_archive(local_path)
 
@@ -57,7 +67,8 @@ def download_dataset(teamfiles_dir: str) -> str:
 
         dataset_path = storage_dir
     return dataset_path
-    
+
+
 def count_files(path, extension):
     count = 0
     for root, dirs, files in os.walk(path):
@@ -65,21 +76,130 @@ def count_files(path, extension):
             if file.endswith(extension):
                 count += 1
     return count
-    
+
+
 def convert_and_upload_supervisely_project(
     api: sly.Api, workspace_id: int, project_name: str
 ) -> sly.ProjectInfo:
     ### Function should read local dataset and upload it to Supervisely project, then return project info.###
-    raise NotImplementedError("The converter should be implemented manually.")
+    dataset_path = "/home/alex/DATASETS/TODO/DeepSportradar/archive"
+    batch_size = 5
+    group_tag_name = "im id"
 
-    # dataset_path = "/local/path/to/your/dataset" # general way
-    # dataset_path = download_dataset(teamfiles_dir) # for large datasets stored on instance
+    train_folders = [
+        "ks-fr-stchamond",
+        "ks-fr-fos",
+        "ks-fr-strasbourg",
+        "ks-fr-vichy",
+        "ks-fr-nantes",
+        "ks-fr-bourgeb",
+        "ks-fr-gravelines",
+        "ks-fr-monaco",
+        "ks-fr-poitiers",
+        "ks-fr-nancy",
+        "ks-fr-lemans",
+    ]
+    val_folders = ["ks-fr-blois"]
+    test_folders = ["ks-fr-caen", "ks-fr-roanne", "ks-fr-limoges"]
+    ds_name_to_folders = {"train": train_folders, "val": val_folders, "test": test_folders}
 
-    # ... some code here ...
+    def create_ann(image_path):
+        labels = []
+        tags = []
 
-    # sly.logger.info('Deleting temporary app storage files...')
-    # shutil.rmtree(storage_dir)
+        image_np = sly.imaging.image.read(image_path)[:, :, 0]
+        img_height = image_np.shape[0]
+        img_wight = image_np.shape[1]
 
-    # return project
+        im_name = get_file_name(image_path)
 
+        id_data = im_name.split("_")[1]
+        group_id = sly.Tag(group_tag_meta, value=id_data)
+        tags.append(group_id)
 
+        seq_val = int(image_path.split("/")[-2])
+        seq = sly.Tag(seq_meta, value=seq_val)
+        tags.append(seq)
+
+        court_tag_meta = name_to_tag[im_name.split("_")[0]]
+        court_tag = sly.Tag(court_tag_meta)
+        tags.append(court_tag)
+
+        im_path = ("/").join(image_path.split("/")[:-1])
+        mask_name = ("_").join(im_name.split("_")[:-1])
+
+        mask_path = os.path.join(im_path, mask_name + "_humans.png")
+        if file_exists(mask_path):
+            mask_np = sly.imaging.image.read(mask_path)[:, :, 0]
+            if len(np.unique(mask_np)) > 1:
+                unique_pixels = np.unique(mask_np)[1:]
+
+                for pixel in unique_pixels:
+                    obj_class = pixel_to_class.get(pixel)
+                    mask = mask_np == pixel
+                    if pixel == 3:
+                        ret, curr_mask = connectedComponents(mask.astype("uint8"), connectivity=8)
+                        for i in range(1, ret):
+                            obj_mask = curr_mask == i
+                            curr_bitmap = sly.Bitmap(obj_mask)
+                            if curr_bitmap.area > 15:
+                                curr_label = sly.Label(curr_bitmap, obj_class)
+                                labels.append(curr_label)
+                    else:
+                        curr_bitmap = sly.Bitmap(mask)
+                        curr_label = sly.Label(curr_bitmap, obj_class)
+                        labels.append(curr_label)
+
+        return sly.Annotation(img_size=(img_height, img_wight), labels=labels, img_tags=tags)
+
+    human = sly.ObjClass("human", sly.Bitmap)
+    ball = sly.ObjClass("ball", sly.Bitmap)
+
+    pixel_to_class = {3: human, 11: ball}
+
+    camcourt1 = sly.TagMeta("camcourt 1", sly.TagValueType.NONE)
+    camcourt2 = sly.TagMeta("camcourt 2", sly.TagValueType.NONE)
+    seq_meta = sly.TagMeta("sequence", sly.TagValueType.ANY_NUMBER)
+
+    name_to_tag = {"camcourt1": camcourt1, "camcourt2": camcourt2}
+
+    group_tag_meta = sly.TagMeta(group_tag_name, sly.TagValueType.ANY_STRING)
+
+    project = api.project.create(workspace_id, project_name, change_name_if_conflict=True)
+    meta = sly.ProjectMeta(
+        obj_classes=[human, ball], tag_metas=[group_tag_meta, camcourt1, camcourt2, seq_meta]
+    )
+    api.project.update_meta(project.id, meta.to_json())
+    api.project.images_grouping(id=project.id, enable=True, tag_name=group_tag_name)
+
+    for ds_name, folders in ds_name_to_folders.items():
+
+        dataset = api.dataset.create(project.id, ds_name, change_name_if_conflict=True)
+
+        for curr_folder in folders:
+
+            curr_path = os.path.join(dataset_path, curr_folder.upper())
+
+            all_images_pathes = glob.glob(curr_path + "/*/*.png")
+
+            images_pathes = []
+            # images_names = []
+            for im_path in all_images_pathes:
+                if get_file_ext(im_path) != ".json" and "_humans" not in im_path:
+                    # images_names.append(get_file_name_with_ext(im_path))
+                    images_pathes.append(im_path)
+
+            progress = sly.Progress("Create dataset {}".format(ds_name), len(images_pathes))
+
+            for img_pathes_batch in sly.batched(images_pathes, batch_size=batch_size):
+                img_names_batch = [get_file_name_with_ext(im_path) for im_path in img_pathes_batch]
+
+                img_infos = api.image.upload_paths(dataset.id, img_names_batch, img_pathes_batch)
+                img_ids = [im_info.id for im_info in img_infos]
+
+                anns = [create_ann(image_path) for image_path in img_pathes_batch]
+                api.annotation.upload_anns(img_ids, anns)
+
+                progress.iters_done_report(len(img_pathes_batch))
+
+    return project
--- src/settings.py
+++ src/settings.py
@@ -13,37 +13,39 @@ from dataset_tools.templates import (
 ##################################
 # * Before uploading to instance #
 ##################################
-PROJECT_NAME: str = None
-PROJECT_NAME_FULL: str = None
+PROJECT_NAME: str = "DeepSportRadar"
+PROJECT_NAME_FULL: str = "DeepSportRadar Instance Segmentation Challenge v.2"
 HIDE_DATASET = True  # set False when 100% sure about repo quality
 
 ##################################
 # * After uploading to instance ##
 ##################################
-LICENSE: License = None
-APPLICATIONS: List[Union[Industry, Domain, Research]] = None
-CATEGORY: Category = None
+LICENSE: License = License.Apache_2_0(
+    source_url="https://github.com/DeepSportradar/instance-segmentation-challenge/blob/master/LICENSE"
+)
+APPLICATIONS: List[Union[Industry, Domain, Research]] = [Industry.Sports()]
+CATEGORY: Category = Category.Sports()
 
-CV_TASKS: List[CVTask] = None
-ANNOTATION_TYPES: List[AnnotationType] = None
+CV_TASKS: List[CVTask] = [CVTask.InstanceSegmentation(), CVTask.SemanticSegmentation(), CVTask.ObjectDetection()]
+ANNOTATION_TYPES: List[AnnotationType] = [AnnotationType.InstanceSegmentation()]
 
-RELEASE_DATE: Optional[str] = None  # e.g. "YYYY-MM-DD"
+RELEASE_DATE: Optional[str] = "2022-08-17"  # e.g. "YYYY-MM-DD"
 if RELEASE_DATE is None:
     RELEASE_YEAR: int = None
 
-HOMEPAGE_URL: str = None
+HOMEPAGE_URL: str = https://github.com/DeepSportradar/instance-segmentation-challenge
 # e.g. "https://some.com/dataset/homepage"
 
-PREVIEW_IMAGE_ID: int = None
+PREVIEW_IMAGE_ID: int = 14347174
 # This should be filled AFTER uploading images to instance, just ID of any image.
 
-GITHUB_URL: str = None
+GITHUB_URL: str = "https://github.com/dataset-ninja/deep-sport-radar"
 # URL to GitHub repo on dataset ninja (e.g. "https://github.com/dataset-ninja/some-dataset")
 
 ##################################
 ### * Optional after uploading ###
 ##################################
-DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = None
+DOWNLOAD_ORIGINAL_URL: Optional[Union[str, dict]] = "https://www.kaggle.com/datasets/deepsportradar/basketball-instants-dataset/download?datasetVersionNumber=4"
 # Optional link for downloading original dataset (e.g. "https://some.com/dataset/download")
 
 CLASS2COLOR: Optional[Dict[str, List[str]]] = None
@@ -51,19 +53,19 @@ CLASS2COLOR: Optional[Dict[str, List[str]]] = None
 
 # If you have more than the one paper, put the most relatable link as the first element of the list
 # Use dict key to specify name for a button
-PAPER: Optional[Union[str, List[str], Dict[str, str]]] = None
+PAPER: Optional[Union[str, List[str], Dict[str, str]]] = "https://arxiv.org/pdf/2208.08190"
 BLOGPOST: Optional[Union[str, List[str], Dict[str, str]]] = None
-REPOSITORY: Optional[Union[str, List[str], Dict[str, str]]] = {"GitHub":"some_link_to_repo_if_exists"}
+REPOSITORY: Optional[Union[str, List[str], Dict[str, str]]] = None
 
 CITATION_URL: Optional[str] = None
-AUTHORS: Optional[List[str]] = None
-AUTHORS_CONTACTS: Optional[List[str]] = None
+AUTHORS: Optional[List[str]] = ["Gabriel Van Zandycke", "Vladimir Somers", "Maxime Istasse", "Carlo Del Don", "Davide Zambrano"]
+AUTHORS_CONTACTS: Optional[List[str]] = ["g.vanzandycke@sportradar.com", "v.somers@sportradar.com", "m.istasse@sportradar.com", "c.deldon@sportradar.com", "d.zambrano@sportradar.com"]
 
-ORGANIZATION_NAME: Optional[Union[str, List[str]]] = None
-ORGANIZATION_URL: Optional[Union[str, List[str]]] = None
+ORGANIZATION_NAME: Optional[Union[str, List[str]]] = "Sportradar AG, Switzerland"
+ORGANIZATION_URL: Optional[Union[str, List[str]]] = "https://sportradar.com/"
 
 # Set '__PRETEXT__' or '__POSTTEXT__' as a key with string value to add custom text. e.g. SLYTAGSPLIT = {'__POSTTEXT__':'some text}
-SLYTAGSPLIT: Optional[Dict[str, Union[List[str], str]]] = None
+SLYTAGSPLIT: Optional[Dict[str, Union[List[str], str]]] = {"2 courts":["camcourt 1", "camcourt 2"], "__POSTTEXT__": "Additionally, every image marked with its ***sequence*** tag"}
 TAGS: Optional[List[str]] = None
 
 
